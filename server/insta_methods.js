034c02c4ff0ed7a3a589fa28fb37508c4bad4c23
Nilansh Bansal <bansalnilansh@gmail.com>
2018-03-18 14:57:55 +0530

    finding insta_business id for all accounts & reduced one api call

--- server/insta_methods.js
+++ server/insta_methods.js
@@ -2,47 +2,47 @@ import { Meteor } from "meteor/meteor";
 import { HTTP } from "meteor/http";
 
 Meteor.methods({
-    "get_page_id"(longToken) {
+
+    "get_insta_business_id"(token) {
         let baseURL = "https://graph.facebook.com/v2.12/";
-        let pagePath = "me/accounts?";
-        let apiURL = `${baseURL}${pagePath}access_token=${longToken}`;
+        let pagePath = `me/accounts?fields=instagram_business_account`;
+        let apiURL = `${baseURL}${pagePath}&access_token=${token}`;
         let res = HTTP.call("get", apiURL);
-        return res;
-    },
-    "get_insta_business_id"(fbPageId,token){
-        let baseURL = "https://graph.facebook.com/v2.12/";
-        let pagePath=`?fields=instagram_business_account`;
-        let apiURL=`${baseURL}${fbPageId}${pagePath}&access_token=${token}`;
-        let res=HTTP.call("get",apiURL);
+
         return res;
     },
 
     "get_page_insights"(pageName, token) {
         let baseURL = "https://graph.facebook.com/v2.12/";
-        let pagePath=`?fields=business_discovery.username(${pageName}){followers_count,media_count,media{comments_count,comment,like_count,timestamp,caption}}`
+        let pagePath = `?fields=business_discovery.username(${pageName}){followers_count,media_count,media{comments_count,comment,like_count,timestamp,caption}}`
         let instaBusinessID;
-        try {
-            let res = Meteor.call("get_page_id", token);
-            if(res.data && res.data.data && res.data.data[0] ){
-            let fbPageId=res.data.data[0].id;
-            try{
-                let response=Meteor.call("get_insta_business_id",fbPageId,token);
-                if(response && response.data && response.data.instagram_business_account)
-                    instaBusinessID=response.data.instagram_business_account.id;
-            } 
-            catch(error){
-                throw new Meteor.Error("error");
-            
-            }
-            let apiURL=`${baseURL}${instaBusinessID}${pagePath}&access_token=${token}`;
-                let result= HTTP.call("get",apiURL);
 
-                return result;
+        try {
+            let response = Meteor.call("get_insta_business_id", token);
+
+            if (response.data && response.data.data) {
+                response.data.data.forEach(eachAccount => {
+                    if (eachAccount.instagram_business_account) {
+                        instaBusinessID = eachAccount.instagram_business_account.id;
+                    }
+                });
+                try {
+                    let apiURL = `${baseURL}${instaBusinessID}${pagePath}&access_token=${token}`;
+                    let result = HTTP.call("get", apiURL);
+
+                    return result;
+                }
+                catch (err) {
+                    throw new Meteor.Error(error.response.data.error.message);
+                }
             }
-            
         }
-        catch (err) {
-             throw new Meteor.Error(err.response.data.error.error_user_msg);
+        catch (error) {
+            throw new Meteor.Error(error.response.data.error.message);
+
         }
+
     }
+
+
 })
